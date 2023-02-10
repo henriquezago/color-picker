f9899e1581002c7e37aeef8081f7da753b7addb8
Henrique Zago <lobato.zago@gmail.com>
2023-02-10 11:46:27 -0300

    chore: removing "greetings" example code

--- migrations/20190724092050_populate_greetings.js
+++
@@ -1,9 +0,0 @@
-exports.up = function (knex) {
-  return knex("greetings").insert([
-    { body: "Welcome to Kickoff Color Picker!" },
-  ]);
-};
-
-exports.down = function (knex) {
-  return knex("greetings").delete();
-};
