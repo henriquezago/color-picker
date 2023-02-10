acaad9ed622391c5b20bfa8898abc08fa7b658e9
Henrique Zago <lobato.zago@gmail.com>
2023-02-10 10:34:35 -0300

    chore: change js files to ts

---
+++ pages/api/palette.ts
@@ -0,0 +1,35 @@
+import knex from "../../clients/knex";
+
+const TABLE = "palette";
+
+export interface RGB {
+  id?: number;
+  red: number;
+  green: number;
+  blue: number;
+}
+
+export default async (req, res) => {
+  if (req.method === "GET") {
+    const colors: RGB[] = await knex(TABLE);
+    res.status(200).json(colors);
+
+  } else if (req.method === "POST") {
+    const color = await knex(TABLE).insert(req.body);
+    res.status(200).json(color);
+
+  } else if (req.method === "PUT") {
+    await knex(TABLE)
+      .where({ id: req.body.id })
+      .update({ body: req.body.body });
+
+    const [color] = await knex(TABLE).where({ id: req.body.id }).limit(1);
+    res.status(200).json(color);
+
+  } else if (req.method === "DELETE") {
+    await knex(TABLE).where({ id: req.body }).del();
+
+  } else {
+    res.status(404).json({ error: `${req.method} endpoint does not exist` });
+  }
+};
