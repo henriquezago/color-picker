c023488628480955b842a35a0d5ae294928450eb
Henrique Zago <lobato.zago@gmail.com>
2023-02-10 10:53:20 -0300

    api(palette): delete color request

--- pages/api/palette.ts
+++ pages/api/palette.ts
@@ -1,3 +1,5 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
 import knex from "../../clients/knex";
 
 const TABLE = "palette";
@@ -9,15 +11,13 @@ export interface RGB {
   blue: number;
 }
 
-export default async (req, res) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "GET") {
     const colors: RGB[] = await knex(TABLE);
     res.status(200).json(colors);
-
   } else if (req.method === "POST") {
     const color = await knex(TABLE).insert(req.body);
     res.status(200).json(color);
-
   } else if (req.method === "PUT") {
     await knex(TABLE)
       .where({ id: req.body.id })
@@ -25,10 +25,9 @@ export default async (req, res) => {
 
     const [color] = await knex(TABLE).where({ id: req.body.id }).limit(1);
     res.status(200).json(color);
-
   } else if (req.method === "DELETE") {
-    await knex(TABLE).where({ id: req.body }).del();
-
+    await knex(TABLE).where({ id: req.body }).delete();
+    res.status(200).json({ id: req.body });
   } else {
     res.status(404).json({ error: `${req.method} endpoint does not exist` });
   }
